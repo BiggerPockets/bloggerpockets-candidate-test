645a193f21801c08727602727d40bf532092767f
luismetzger <luismetzger@me.com>
2019-06-21 12:23:18 -0600

    Format is html by default.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
+
   def index
     @posts = Post.where(published: true)
 
@@ -9,12 +11,16 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def comments
     @post = Post.find(params[:id])
     @comments = @post.comments
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
