e2a5f503897704ba2d123ce215d1236b981e4a40
luismetzger <luismetzger@me.com>
2019-06-21 12:30:39 -0600

    A little better to render JSON. I can do better.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @posts = @user.posts
+  before_action :find_user, only: [:show]
 
+  def show
+    @posts = @user.posts
     respond_to do |format|
       format.html
-      format.json { render json: @user }
+      format.json { render json: {
+                                    data: {
+                                            user: @user
+                                          }
+                                  } }
     end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
