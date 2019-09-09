8f818b34bcedef4397de53dc1e9bd1c7023bb28e
bghalami <benjaminghalami@gmail.com>
2019-09-09 13:37:45 -0600

    Mirrored associations for readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  # has_many :comments isn't necessary here, as I'm never making a user.comments call
+  # but I find it helps the readability of the project
+  has_many :comments
 end
